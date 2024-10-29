49386eb4f88608a84dfd10c31ca8007e4012264c
clemlak <clementlakhal@gmail.com>
2024-10-29 12:58:24 +0400

    chore: rename default Counter contract to Core

---
+++ src/Core.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+struct Player {
+    bool hasEntered;
+    uint8 health;
+}
+
+contract Core {
+    function enter() external {}
+}
