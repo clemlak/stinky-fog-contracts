d1c67538b7e774612656e1840ee423f06c20a664
clemlak <clementlakhal@gmail.com>
2024-11-29 14:27:48 +0400

    feat: add Status, Kontract types

--- src/Core.sol
+++ src/Core.sol
@@ -1,4 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+enum Status {
+    Open,
+    Disputed,
+    Closed
+}
+
+struct Kontract {
+    address creator;
+    address worker;
+    address escrow;
+    address currency;
+    uint256 amount;
+    uint256 amountPaid;
+    uint256 deadline;
+    Status status;
+}
+
 contract Core {}
