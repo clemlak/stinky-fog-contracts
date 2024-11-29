a5c4e61bcb1988be15e647391302b9a137f0d05b
clemlak <clementlakhal@gmail.com>
2024-11-29 14:23:28 +0400

    feat: clean up Core contract

--- src/Core.sol
+++ src/Core.sol
@@ -1,11 +1,4 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-struct Player {
-    bool hasEntered;
-    uint8 health;
-}
-
-contract Core {
-    function enter() external {}
-}
+contract Core {}
